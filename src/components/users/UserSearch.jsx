2a9b8f99f1d5edc9e39ea8942df36e6c1ef31f34
bpmanzo <millen.brent@gmail.com>
2023-01-06 23:21:57 +0800

    Set alert

--- src/components/users/UserSearch.jsx
+++ src/components/users/UserSearch.jsx
@@ -1,19 +1,23 @@
 import { useState, useContext } from "react";
 import GithubContext from "../../context/github/GithubContext";
+import AlertContext from "../../context/alert/AlertContext";
 
 function UserSearch() {
   const [text, setText] = useState("");
 
   const { users, searchUsers, clearUsers } = useContext(GithubContext);
+  const { setAlert } = useContext(AlertContext);
+
   const handleChange = (e) => setText(e.target.value);
 
   const handleSubmit = (e) => {
     e.preventDefault();
 
     if (text === "") {
-      alert("Please enter user");
+      setAlert("Please enter something", "error");
     } else {
       searchUsers(text);
+
       setText("");
     }
   };
@@ -21,7 +25,7 @@ function UserSearch() {
   return (
     <div className="grid grid-cols-1 xl:grid-cols-2 lg:grid-cols-2 md:grid-cols-2 mb-8 gap-8">
       <div>
-        <form onSubmit={handleSubmit} action="">
+        <form onSubmit={handleSubmit}>
           <div className="form-control">
             <div className="relative">
               <input
